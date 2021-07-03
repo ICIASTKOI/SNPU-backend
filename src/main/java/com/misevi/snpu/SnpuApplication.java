ffc817bd47f445b4eddffe1ef4fa4493bf05543a
Hubert Szczęch <hubertszczech@MacBook-Pro-Hubert.local>
2021-07-04 00:14:54 +0200

    initial project

---
+++ src/main/java/com/misevi/snpu/SnpuApplication.java
@@ -0,0 +1,13 @@
+package com.misevi.snpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnpuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnpuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misevi/snpu/SnpuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misevi.snpu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnpuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
